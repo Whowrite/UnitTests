829791204403db202085120ea64388c773f958a6
Mikki21 <mikki.s.fails@gmail.com>
2024-11-10 17:26:13 +0200

    v0.1

---
+++ var1/NotificationService.js
@@ -0,0 +1,8 @@
+class NotificationService {
+    async sendWelcomeMessage(userId, userName) {
+      console.log(`Welcome message sent to ${userName} (ID: ${userId})`);
+    }
+  }
+  
+  export default NotificationService;
+  
